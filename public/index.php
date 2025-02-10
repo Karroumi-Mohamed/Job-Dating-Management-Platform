0ecd284f5271fafc2da5d0054f235bdaa207b267
Mohamed Karroumi <mkaroumi123@gmail.com>
2025-02-10 10:50:26 +0100

    adding the mvc tamplate

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+require "../vendor/larapack/dd/src/helper.php";
+include '../vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use App\Core\Database;
+use App\Core\Session;
+use Dotenv\Dotenv;
+use App\Controllers\AuthController;
+use App\Controllers\HomeController;
+use App\Core\Router;
+
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+Session::start();
+Database::init();
+
+
+$r = new Router();
+$r->get('/login', [AuthController::class, 'showLogin']);
+$r->post('/login', [AuthController::class, 'login']);
+$r->get('/logout', [AuthController::class, 'logout']);
+$r->get('/register', [AuthController::class, 'showRegister']);
+$r->post('/register', [AuthController::class, 'register']);
+
+$r->get('/', [HomeController::class, 'index']);
+$r->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
