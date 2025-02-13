e0d809e427dc6f8a2d0beef6d8afad82f82b6885
Mohamed Karroumi <mkaroumi123@gmail.com>
2025-02-13 02:43:19 +0100

    WQ-26 fixed lougout route doesnt work

--- config/routes.php
+++ config/routes.php
@@ -4,36 +4,36 @@ use App\Controllers\AnnoncementsController;
 use App\Controllers\AuthController;
 use App\Controllers\CompaniesController;
 use App\Controllers\ListAnnouncesController;
+use App\Core\Auth;
 use App\Core\Router;
 
-// Auth routes
 Router::get('/login', [AuthController::class, 'showLogin']);
 Router::get('/register', [AuthController::class, 'showRegister']);
 Router::post('/register', [AuthController::class, 'register']);
 Router::post('/login', [AuthController::class, 'login']);
-Router::get('/logout', [AuthController::class, 'logout']);
+Router::get('/logout', function () {
+    Auth::logout();
+    header('Location: /login');
+    exit;
+});
 
-// Companies routes
 Router::get('/companies', [CompaniesController::class, 'index']);
+Router::get('/companies/create', [CompaniesController::class, 'create']);
+Router::post('/companies/create', [CompaniesController::class, 'store']);
+Router::get('/companies/edit/{id}', [CompaniesController::class, 'edit']);
+Router::post('/companies/edit/{id}', [CompaniesController::class, 'update']);
+Router::post('/companies/delete/{id}', [CompaniesController::class, 'delete']);
+Router::post('/companies/restore/{id}', [CompaniesController::class, 'restore']);
 Router::get('/companies/trash', [CompaniesController::class, 'trash']);
-Router::post('/companies/restore/{id}', [CompaniesController::class, 'restore']); // Add this line
 
-// Companies API routes
-Router::get('/api/companies', [CompaniesController::class, 'getCompaniesTable']);
-Router::get('/api/companies/trash', [CompaniesController::class, 'getTrashTable']); // Add this line
-Router::post('/api/companies/store', [CompaniesController::class, 'store']);
-Router::post('/api/companies/update/{id}', [CompaniesController::class, 'update']);
-Router::post('/api/companies/delete/{id}', [CompaniesController::class, 'delete']);
-Router::post('/api/companies/restore/{id}', [CompaniesController::class, 'restore']);
+Router::get('/announcements/trash', [AnnoncementsController::class, 'trash']);
+
+Router::post('/announcements/restore/{id}', [AnnoncementsController::class, 'restore']);
 
 Router::get('/', [ListAnnouncesController::class, 'index']);
 Router::get('/announcements', [AnnoncementsController::class, 'index']);
-Router::get('/announcements/trash', [AnnoncementsController::class, 'trash']);
-
-// api nta3 ajax
-Router::get('/api/announcements', [AnnoncementsController::class, 'getAnnouncementsTable']);
-Router::get('/api/announcements/trash', [AnnoncementsController::class, 'getTrashTable']);
-Router::post('/api/announcements/store', [AnnoncementsController::class, 'store']);
-Router::post('/api/announcements/update/{id}', [AnnoncementsController::class, 'update']);
-Router::post('/api/announcements/delete/{id}', [AnnoncementsController::class, 'delete']);
-Router::post('/api/announcements/restore/{id}', [AnnoncementsController::class, 'restore']);
+Router::get('/announcements/create', [AnnoncementsController::class, 'create']);
+Router::post('/announcements/store', [AnnoncementsController::class, 'store']);
+Router::get('/announcements/edit/{id}', [AnnoncementsController::class, 'edit']);
+Router::post('/announcements/update/{id}', [AnnoncementsController::class, 'update']);
+Router::post('/announcements/delete/{id}', [AnnoncementsController::class, 'delete']);
